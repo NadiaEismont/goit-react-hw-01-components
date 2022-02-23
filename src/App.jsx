78ca6165dc39947b9697702e9026991a04ef75e4
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-02-23 20:00:59 +0200

    first task

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,15 @@ export const App = () => {
         color: '#010101',
       }}
     >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
       React homework template
     </div>
   );
