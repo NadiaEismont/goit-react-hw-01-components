85830b3e4f4d560b1e5f98a4e59bc86f1f9d0e69
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-03-09 15:53:20 +0200

    task2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
 import user from 'DATA/user.json';
 import Profile from 'components/Profile';
+import Statistics from 'components/Statistics';
+import statistic from 'DATA/statistic.json';
 import 'core-js/es';
+
+const Section = ({ title, children }) => (
+  <section>
+    <h2>{title}</h2>
+    {children}
+  </section>
+);
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-      React homework template
+    <div>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          textTransform: 'uppercase',
+          color: '#010101',
+        }}
+      >
+        <Section title={"Task-1 'Social network profile'"}>
+          <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+          />
+        </Section>
+      </div>
+      <Section title={"Task-2 'Statistics section'"}>
+        <Statistics title="Upload stats" stats={statistic} />;
+      </Section>
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import style from './profile.module.css';
+
+export default function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <div className={style.profile}>
+      <div className={style.description}>
+        <img src={avatar} alt="User avatar" className={style.avatar} />
+        <p className={style.name}>{username}</p>
+        <p className={style.tag}>{tag}</p>
+        <p className={style.location}>{location}</p>
+      </div>
+
+      <ul className={style.stats}>
+        <li className={style.item}>
+          <span className={style.label}>Followers</span>
+          <span className={style.quantity}>{stats.followers}</span>
+        </li>
+        <li className={style.item}>
+          <span className={style.label}>Views</span>
+          <span className={style.quantity}>{stats.views}</span>
+        </li>
+        <li className={style.item}>
+          <span className={style.label}>Likes</span>
+          <span className={style.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.prototype = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
